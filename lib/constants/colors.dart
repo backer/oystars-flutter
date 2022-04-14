81d871557271dfdd4b5550ddf99444aa1e6a6d18
Brian Acker <backer@brightcove.com>
2022-04-14 17:21:55 -0400

    Store iOS logo hex color
    just in case we need it later

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,1 +1,4 @@
 const LOGO_BACKGROUND_COLOR = 0xff00a5cf;
+// iOS uses different/offset hex values compared to flutter and Android Studio
+// for some reason, so include it here just in case we need it
+const LOGO_BACKGROUND_COLOR_IOS = 0xff0097C2;

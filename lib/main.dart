7e378da78555a5c0b6a373062f3d01c3db319a28
Brian Acker <backer@brightcove.com>
2022-04-17 00:49:38 -0400

    change material app title to app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oystars_flutter_app/constants/strings.dart';
 import 'package:oystars_flutter_app/routes/splash.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: APP_NAME,
       theme: ThemeData(
         // This is the theme of your application.
         //

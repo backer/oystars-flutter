6c3683c3e910da41d9d6a53dedb04b9b90347583
Brian Acker <backer@brightcove.com>
2022-08-02 18:09:01 -0400

    support imageUrl in push notifications on ios

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Brian Acker on 8/2/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
